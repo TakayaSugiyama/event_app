5943dbb8cb24f57cb8357c6e36882235e096e11b
takayasugiyama <s.takaya1027@gmail.com>
2019-11-09 01:22:24 +0900

    GitHubアカウントでのログイン・登録機能を実装

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -32,6 +32,7 @@ Devise.setup do |config|
   # available as additional gems.
   require 'devise/orm/active_record'
 
+  config.omniauth :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'],scope: "user,repo,gist"
   # ==> Configuration for any authentication mechanism
   # Configure which keys are used when authenticating a user. The default is
   # just :email. You can configure it to use [:username, :subdomain], so for
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-  devise_for :users
   root to: 'welcome#index'
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks'}
+  resource :users
 end
