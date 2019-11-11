a73d22e49e61ff546d186dd3277cf887167826f0
takayasugiyama <s.takaya1027@gmail.com>
2019-11-11 14:12:56 +0900

    ticetモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_11_031317) do
+ActiveRecord::Schema.define(version: 2019_11_11_051032) do
 
   create_table "events", force: :cascade do |t|
     t.integer "owner_id"
@@ -21,6 +21,16 @@ ActiveRecord::Schema.define(version: 2019_11_11_031317) do
     t.text "content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "tickets", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "event_id", null: false
+    t.string "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_tickets_on_event_id"
+    t.index ["user_id"], name: "index_tickets_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
