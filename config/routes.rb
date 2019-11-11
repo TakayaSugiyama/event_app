b2daa664dfbb7fcc47fd278422e57b41c1eb2016
takayasugiyama <s.takaya1027@gmail.com>
2019-11-11 10:35:43 +0900

    event modelとevents controllerを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :events
   root to: 'welcome#index'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks'}
