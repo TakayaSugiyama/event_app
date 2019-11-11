33b657bd078b22dc732a4858cf48161122c62550
takayasugiyama <s.takaya1027@gmail.com>
2019-11-11 11:00:48 +0900

    newとcreateアクションの実装

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,21 @@
 class EventsController < ApplicationController
+  def new 
+    @event = current_user.events.build
+  end
+
+  def create 
+    @event = current_user.events.build(event_params)
+    if @event.save 
+      redirect_to @event, notice: "作成しました"
+    else 
+      render :new
+    end
+  end
+
+  private 
+
+  def event_params 
+    params.require(:event).permit(:name,:place,:start_time,:end_time, :content)
+  end
+
 end
