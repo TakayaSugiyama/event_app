09a422b94d5e4315931b3700f36cdb3387a3e836
takayasugiyama <s.takaya1027@gmail.com>
2019-11-11 10:45:10 +0900

    eventモデルのバリデーションを設定

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,17 @@
 class Event < ApplicationRecord
+  validates :name, presence: true, length: {maximum: 50}
+  validates :place, presence: true, length: {maximum: 100}
+  validates :content, presence: true , length: {maximum:2000}
+  validates :start_time, presence: true
+  validates :end_time, presence: true
+  validates :start_time_should_be_before_end_time
+
+  private 
+
+  def start_time_should_be_before_end_time 
+    return unless start_time && end_time
+    if start_time >= end_time
+       errors.add(:start_time, "終了時間よりも前に設定してください")
+    end
+  end
 end
