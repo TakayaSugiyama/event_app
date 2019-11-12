4af4d20d4acf61f7cec044f328f613f2520592e0
takayasugiyama <s.takaya1027@gmail.com>
2019-11-12 11:30:48 +0900

    ホーム画面などを実装

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   root to: 'welcome#index'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks'}
-  resource :users
+  resources :users
 end
