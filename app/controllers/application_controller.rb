7cc74b8c2108b54a2e3c19c20be1b78aab57a03d
takayasugiyama <s.takaya1027@gmail.com>
2019-11-12 14:40:58 +0900

    エラーハンドリングを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,19 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery prepend: true ,with: :exception
+
+  rescue_from Exception, with: :error500
+  rescue_from ActiveRecord::RecordNotFound, ActionController::RoutingError, with: :error404
+ 
+  def error404(e)
+    render "error404", status: 404, formats: [:html]
+  end
+
+  def error500(e)
+    logger.error [e, *e.backtrace].join('¥')
+    render "error500", status: 500, formats: [:html]
+  end
+
+
   def after_sign_in_path_for(resource)
     user_path(current_user)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,6 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks'}
   resources :users
+  
+  match "*path" , to: "application#error404", via: :all
 end
